2e4bec65569f0cbb910bc625f4d413490e8f00cc
Emilmrk <93278758+Emilmrk@users.noreply.github.com>
2022-07-12 22:44:46 -0500

    Update Exercise1.java

--- src/main/java/com/example/project/Exercise1.java
+++ src/main/java/com/example/project/Exercise1.java
@@ -20,23 +20,23 @@ public class Exercise1 {
     }
 
 
-     public <T extends Comparable<T>> boolean bstSimilares(BST<T> a1, BST<T> a2){
+      public <T extends Comparable<T>> boolean bstSimilares(BST<T> a1, BST<T> a2){
     		
     	if(a1.isEmpty()&&a2.isEmpty()) {
     		return true;
     	}else {
-//    		if (value(a1.root)==value(a2.root)) {
-//			return value(a1.root.right)==value(a2.root.right)&&value(a1.root.left)==value(a2.root.left);
-//    			
-//			}else {
-//				return false;
-//			}
     		return similares(a1.root, a2.root);
     	}
     }
     public <T extends Comparable<T>> boolean similares(Node<T> a1, Node<T> a2) {
     	if (value(a1)==value(a2)) {
-    		//return value(a1.right)==value(a2.right)&&value(a1.left)==value(a2.left);
+    		if (similares(a1.left, a2.left)&&a1.left==null) {
+    			return similares(a1.right, a2.right);
+			} 
+    		if (similares(a1.right, a2.right)&&a1.right==null) {
+    			return similares(a1.left, a2.left);
+			}
+
     		return similares(a1.left, a2.left)&&similares(a1.right, a2.right);
 
     	}else {
